17c62409eedf8b4f454f71b9c19dad5a03262109
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2025-05-09 10:33:07 -0400

    Update the styling of the buttons to have a consistent height

--- js/components/paper-button.js
+++ js/components/paper-button.js
@@ -4,31 +4,41 @@ const PAPER_BUTTON_CODE_TEMPLATE = `
 </button>`;
 const PAPER_BUTTON_ID = "paper-button";
 const PAPER_BUTTON_STYLE = `
-  .paper-button {
-    font-family: ${Font.DEFAULT_FAMILY};
-    background-color: ${Colors.BUTTON_BACKGROUND};
-    border: none;
-    border-radius: ${DimensionsPx.SMALL};
-    color: ${Colors.BUTTON_TEXT};
-    cursor: pointer;
-    font-size: 1em;
-    padding: ${DimensionsPx.MEDIUM} ${DimensionsPx.MLARGE};
-    text-align: center;
-    text-decoration: none;
-    text-transform: uppercase;
-    transition: background-color 0.3s, transform 0.3s;
-    text-shadow: 0 0 ${DimensionsPx.SMALL} ${Colors.PAPER_BACKGROUND}90;
-  }
+:host {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  .paper-button:active {
-    transform: scale(0.95);
-  }
+.paper-button {
+  font-family: ${Font.DEFAULT_FAMILY};
+  background-color: ${Colors.BUTTON_BACKGROUND};
+  border: none;
+  border-radius: ${DimensionsPx.SMALL};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${Colors.BUTTON_TEXT};
+  cursor: pointer;
+  font-size: 1em;
+  height: ${DimensionsPx.LARGE};
+  padding: ${DimensionsPx.MEDIUM} ${DimensionsPx.MLARGE};
+  text-align: center;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: background-color 0.3s, transform 0.3s;
+  text-shadow: 0 0 ${DimensionsPx.SMALL} ${Colors.PAPER_BACKGROUND}90;
+}
 
-  .paper-button:disabled {
-    background-color: ${Colors.BACKGROUND_DISABLED};
-    color: ${Colors.TEXT_DISABLED};
-    cursor: not-allowed;
-  }
+.paper-button:active {
+  transform: scale(0.95);
+}
+
+.paper-button:disabled {
+  background-color: ${Colors.BACKGROUND_DISABLED};
+  color: ${Colors.TEXT_DISABLED};
+  cursor: not-allowed;
+}
 `;
 
 customElements.define(
